4e2a086570bbe3b3c17ff2d97f41dadcb88b22ba
User <User@DESKTOP-TGR46J5>
2025-08-23 16:44:23 -0300

    Estrutura Sequencial: Funções matemáticas em Java

---
+++ funcoes_matematicas/src/funcoes_matematicas/Main.java
@@ -0,0 +1,51 @@
+package funcoes_matematicas;
+
+import java.util.Locale;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		
+		double x = 3.0;
+		double y = 4.0;
+		double z = -5.0;
+		double A, B, C;
+		
+		A = Math.sqrt(x);
+		B = Math.sqrt(y);
+		C = Math.sqrt(25.0);
+		System.out.println("Raiz quadrada de " + x + " = " + A);
+		System.out.println("Raiz quadrada de " + y + " = " + B);
+		System.out.println("Raiz quadrada de 25 = " + C);
+		
+		A = Math.pow(x, y);
+		B = Math.pow(x, 2.0);
+		C = Math.pow(5.0, 2.0);
+		System.out.println(x + " elevado a " + y + " = " + A);
+		System.out.println(x + " elevado ao quadrado = " + B);
+		System.out.println("5 elevado ao quadrado =" + C);
+		
+		A = Math.abs(y);
+		B = Math.abs(z);
+		System.out.println("Valor absoluto de "+ y + " = "+ A);
+		System.out.println("Valor absoluto de "+ z + " = "+ B);
+		System.out.println("--------------------------------------");
+		//Formula de Bakara
+		double a = 1;
+		double b = 2;
+		double c = -3;
+		double delta, x1, x2;
+		
+		delta = Math.pow(b, 2.0) - 4 * a * c;
+		
+		x1 = (-b + Math.sqrt(delta)) / (2.0 * a);
+		x2 = (-b - Math.sqrt(delta)) / (2.0 * a);
+		
+		System.out.println("a raiz 1 = " + x1);
+		System.out.println("a raiz 2 = " + x2);
+
+	}
+
+}
