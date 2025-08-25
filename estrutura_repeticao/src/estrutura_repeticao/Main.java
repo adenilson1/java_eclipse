bf94d6084ba05303575db4c75e1e921e4fefc8c1
User <User@DESKTOP-TGR46J5>
2025-08-25 12:03:04 -0300

    Estrututra Condicional: Estrutura condicional (if-else)

---
+++ estrutura_repeticao/src/estrutura_repeticao/Main.java
@@ -0,0 +1,31 @@
+package estrutura_repeticao;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int hora;
+				
+		System.out.println("Que horas são ?:");
+		hora = sc.nextInt();
+		
+		if(hora < 12) {
+			System.out.println("Bom dia");
+		}
+		else if(hora < 18) {
+				System.out.println("Bom tarde");
+			}
+		else {
+			System.out.println("Boa noite");
+				
+			}	
+			
+		
+		sc.close();
+
+
+	}
+
+}
